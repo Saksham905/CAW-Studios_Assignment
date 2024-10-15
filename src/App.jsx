7ede1381ad20768d113602d846b4bc7c662aa4d2
sakshammishraGla <sakshamgla72@gmail.com>
2024-10-15 11:04:31 +0530

    ui changes in app

--- src/App.jsx
+++ src/App.jsx
@@ -37,15 +37,21 @@ const App = () => {
 
   return (
 
-      <div>
-        <AppContext.Provider value={contextObj}>
-          <h1 className="font-serif text-4xl font-semibold text-center my-1">ToDo App</h1>
-          <AddTask />
-          {tasks.length > 0 ? <TaskFilter /> : null}
-          <Modal />
-          <TaskList />
-        </AppContext.Provider>
-      </div>
+    <div
+      style={{
+        backgroundImage: "url('https://plus.unsplash.com/premium_photo-1683309567810-4d232ee83d2f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8bm90ZXxlbnwwfHwwfHx8MA%3D%3D')",
+        backgroundSize: "cover",
+        backgroundPosition: "center", 
+      }}
+      className="h-screen bg-gray-800 bg-opacity-20 backdrop-blur-md " >
+      <AppContext.Provider value={contextObj}>
+        <h1 className="mb-3 font-serif text-4xl font-semibold text-center">ToDo App</h1>
+        <AddTask />
+        {tasks.length > 0 ? <TaskFilter /> : null}
+        <Modal />
+        <TaskList />
+      </AppContext.Provider>
+    </div>
 
   )
 }
